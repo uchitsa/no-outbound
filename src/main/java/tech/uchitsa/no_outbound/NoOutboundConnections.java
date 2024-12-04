a212619fa1f12db7844b1c820b63241e2e5f97de
uchitsa <uchitsa@ya.ru>
2024-12-05 01:48:08 +0300

    add interface NoOutboundConnections

---
+++ src/main/java/tech/uchitsa/no_outbound/NoOutboundConnections.java
@@ -0,0 +1,12 @@
+package tech.uchitsa.no_outbound;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface NoOutboundConnections {
+}
