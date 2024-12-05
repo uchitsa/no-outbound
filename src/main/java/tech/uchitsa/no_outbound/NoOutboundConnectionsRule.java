e1dfb24e7c67818949584761d0537f9cef1604ca
uchitsa <uchitsa@ya.ru>
2024-12-06 01:30:19 +0300

    update NoOutboundConnectionsRule

--- src/main/java/tech/uchitsa/no_outbound/NoOutboundConnectionsRule.java
+++ src/main/java/tech/uchitsa/no_outbound/NoOutboundConnectionsRule.java
@@ -4,6 +4,9 @@ import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
+import java.lang.reflect.Field;
+import java.net.Socket;
+
 public class NoOutboundConnectionsRule implements TestRule {
     public Statement apply(Statement base, Description desc) {
         NoOutboundConnections annotation = desc.getAnnotation(NoOutboundConnections.class);
@@ -21,7 +24,22 @@ public class NoOutboundConnectionsRule implements TestRule {
         }
 
         public void evaluate() throws Throwable {
-            //TODO
+            try {
+                overrideSocketClass();
+                base.evaluate();
+            } finally {
+                restoreSocketClass();
+            }
+        }
+
+        private void overrideSocketClass() throws NoSuchFieldException {
+            Class<?> socketClass = Socket.class;
+            Field classLoaderField = Class.class.getDeclaredField("classLoader");
+
+        }
+
+
+        private void restoreSocketClass() {
         }
     }
 
